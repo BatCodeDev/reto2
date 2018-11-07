c7776e29beae1b6e8aed59b6c932923340670702
Unknown <rafaestaenclase@gmail.com>
2018-11-07 10:57:21 +0100

    Falta ajax

---
+++ source/loginRegistry.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<style type="text/css">
+		nav{
+			width: 100%;
+			height: 30px;
+			background-color: blue;
+		}
+
+	</style>
+</head>
+<body>
+<nav>asd</nav>
+</body>
+</html>
