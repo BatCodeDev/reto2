91bff92072ae05ca17018f47cfb486cae085daa1
Unknown <rafaestaenclase@gmail.com>
2018-11-08 14:10:54 +0100

    Login terminado

---
+++ source/server/verifyRegistry.php
@@ -0,0 +1,23 @@
+<?php
+    include_once "connection.php";
+    
+    $dbh = connect();
+    $email = $_POST["email"];
+    $user = $_POST["user"];
+    $password = $_POST["password"];
+    $name = $_POST["name"];
+    $surname = $_POST["surname"];
+
+    //Comprobation
+    if(selectIdProfile($dbh, $email)===null){
+        if (selectIdLogin($dbh, $user)===null) {
+            insertRegistryUser($dbh, $email, $user, $password, $name, $surname);
+            echo "success";
+        }else{
+            echo "errorUser";
+        }
+    }else{
+            echo "errorEmail";
+    }
+    closeConnect($dbh);
+?>
