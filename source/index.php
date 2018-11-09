c1ad84df6dc320b0b8f5ef3d59799be3d39b2c2f
Alejandro_Diaz_de_Otalora <alexddo122@gmail.com>
2018-11-09 13:04:30 +0100

    Update index.php

--- source/index.php
+++ source/index.php
@@ -8,7 +8,8 @@
     <title></title>
     <meta name="viewport" content="width=device-width">
     <script src="js/jquery.js"></script>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/grid.css">
     <style>
         #grid{
             display: grid;
@@ -144,30 +145,5 @@
         </footer>
     </div>
 </body>
-<script>
-    $(window).resize(function(){
-       $("body").width($(window).width());
-       $("body").height($(window).height());
-       $("#grid").height($(window).height());
-    });
-    function toggleNavbar() {
-        toggle($('#navBar'), 'toggle');
-        toggle($('#divNavTrigger'), 'trigger');
-        goFade($('#fullfade'));
-    }
-    function toggle(toToggle, toggleClass) {
-        if(toToggle.hasClass(toggleClass)){
-            toToggle.removeClass(toggleClass);
-        }else{
-            toToggle.addClass(toggleClass);
-        }
-    }
-    function goFade(element) {
-        if(!element.is(":visible")){
-            element.fadeIn(200);
-        }else{
-            element.fadeOut(200);
-        }
-    }
-</script>
+<script src="js/main.js"></script>
 </html>
