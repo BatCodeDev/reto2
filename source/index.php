9d6ea7543546d4565a23fe18afab34b6f9d34b19
Unknown <rafaestaenclase@gmail.com>
2018-11-09 13:57:48 +0100

    login nueva BD

--- source/index.php
+++ source/index.php
@@ -1,93 +1,26 @@
-<?php
-    include_once "server/questionDB.php";
-    include_once "server/favouriteDB.php";
-    include_once "server/categoryDB.php";
-    session_start();
-    $userImg = "img/alexddo.png";
-?>
 <!DOCTYPE html>
 <html>
 <head>
     <title></title>
     <meta name="viewport" content="width=device-width">
     <script src="js/jquery.js"></script>
-    <link rel="stylesheet" href="css/style.css">
-    <style>
-        #grid{
-            display: grid;
-            grid-template-columns: repeat(10, 10%);
-            grid-template-rows: repeat(10, 10%);                       
-        }        
-    </style>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/grid.css">
 </head>
 <body>
     <div id="grid">
-        <script>
-            $("body").width($(window).width());
-            $("body").height($(window).height());
-            $("#grid").height($(window).height());
-        </script>
-        <div id="fullfade" onclick="toggleNavbar();"></div>
-        <nav id="navBar">
-            <div id="navBarHeader">
-                <div id="navBarHeaderTxt">
-                    <h2>Menu</h2>
-                </div>
-            </div>
-            <div id="navBarContent">
-                <?php 
-                    if(isset($_SESSION['img'], $_SESSION['user'])){
-                ?>
-                <div class="navLinks">
-                    <!--<img src="img/profile.png">-->
-                    <img id="userImg" src="<?=$_SESSION['img']?>">
-                    <a href="profile.php"><?=$_SESSION['user']?></a>
-                </div>
-                <?php 
-                    }else{
-                ?>                
-                <div class="navLinks">
-                    <!--<img src="img/profile.png">-->
-                    <img id="loginImg" src="img/login.png">
-                    <a href="profile.php">Login/Registro</a>
-                </div>
-                <?php 
-                    }
-                ?>
-                <div class="navLinks">
-                    <img src="img/history.png"">
-                    <a href="">Historial de preguntas</a>
-                </div>
-                <div class="navLinks">
-                    <img src="img/logo.png"">
-                    <a href="">link</a>
-                </div>
-                <div class="navLinks">
-                    <img src="img/logo.png"">
-                    <a href="">link</a>
-                </div>
-            </div>
-        </nav>
-        <header>
-            <div id="divLogo">
-                <img src="img/logo.png">
-            </div>
-            <div id="divSearch">
-                <form method="post">
-                    <input type="text">
-                    <button type="submit" id="search">
-
-                    </button>
-                </form>
-            </div>
-            <div id="divNavTrigger" onclick="toggleNavbar();">
-                <div class="divNavAxis"></div>
-                <div class="divNavAxis"></div>
-                <div class="divNavAxis"></div>
-            </div>
-        </header>
+        <?php 
+            include "navBar.php"; 
+            include "sideBar.php"; 
+            
+            include_once "server/questionDB.php";
+            include_once "server/favouriteDB.php";
+            include_once "server/categoryDB.php";
+            session_start();
+            $userImg = "img/alexddo.png";
+        ?>
         <div id="content">
-            <div class="verticalThird">
+            <div class="divCategory">
                 <div class="verticalHeader">
                     <h2>Categorias:</h2>
                 </div>
@@ -103,67 +36,38 @@
                     ?>
                 </div>
             </div>
-            <div class="verticalThird">
-                <div class="verticalHeader">
-                    <h2>Preguntas recientes:</h2>
-                </div>
-                <div class="verticalTxt">
-                    <?php
-                        $resul = selectRecientQuestion();
-                        foreach ($resul as $row){
-                            echo $row["header"]." ".$row["date"].PHP_EOL."<br>";
-                        }
-                    ?>
-
-                </div>
-            </div>
-            <div class="verticalThird">
-                <div class="verticalHeader">
-                    <h2>Preguntas destacadas:</h2>
-                    <?php
-                        $resulF = selectFavouriteQuestion();
-                        foreach ($resulF as $rowF){
-                            $resulF = selectQuestionById($rowF["id_question"]);
-                            foreach ($resulF as $rowQ){
-                                echo $rowQ["header"]." ".$rowQ["date"].PHP_EOL."<br>";
+            <div id="questions">
+                <div class="verticalHalf">
+                    <div class="verticalHeader">
+                        <h2>Preguntas recientes:</h2>
+                    </div>
+                    <div class="verticalTxt">
+                        <?php
+                            $resul = selectRecientQuestion();
+                            foreach ($resul as $row){
+                                echo $row["header"]." ".$row["date"].PHP_EOL."<br>";
                             }
-                        }
-                    ?>
+                        ?>
+                    </div>
                 </div>
-                <div class="verticalTxt">
-
+                <div class="verticalHalf">
+                    <div class="verticalHeader">
+                        <h2>Preguntas destacadas:</h2>
+                    </div>
+                    <div class="verticalTxt">
+                        <?php
+                            $resulF = selectFavouriteQuestion();
+                            foreach ($resulF as $rowF){
+                                $resulF = selectQuestionById($rowF["id_question"]);
+                                foreach ($resulF as $rowQ){
+                                    echo $rowQ["header"]." ".$rowQ["date"].PHP_EOL."<br>";
+                                }
+                            }
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
-        <footer>
-            footer
-        </footer>
     </div>
 </body>
-<script>
-    $(window).resize(function(){
-       $("body").width($(window).width());
-       $("body").height($(window).height());
-       $("#grid").height($(window).height());
-    });
-    function toggleNavbar() {
-        toggle($('#navBar'), 'toggle');
-        toggle($('#divNavTrigger'), 'trigger');
-        goFade($('#fullfade'));
-    }
-    function toggle(toToggle, toggleClass) {
-        if(toToggle.hasClass(toggleClass)){
-            toToggle.removeClass(toggleClass);
-        }else{
-            toToggle.addClass(toggleClass);
-        }
-    }
-    function goFade(element) {
-        if(!element.is(":visible")){
-            element.fadeIn(200);
-        }else{
-            element.fadeOut(200);
-        }
-    }
-</script>
-</html>
+    <script src="js/main.js"></script></hmtl>
