6a431264f1c9857f52031f81b2ca02027b5d0200
Unknown <rafaestaenclase@gmail.com>
2018-11-13 10:02:14 +0100

    insertCategory

--- source/question.php
+++ source/question.php
@@ -6,6 +6,16 @@
     <script src="js/jquery.js"></script>
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/grid.css">
+    <style type="text/css">
+        #content{
+            background-color: lightblue;
+            margin: 0 auto;
+        }
+
+        .divQuestion{
+            background-color: white;
+        }
+    </style>
 </head>
 <body>
     <div id="grid">
@@ -22,16 +32,43 @@
                 <div class="divQuestion">
                     <h3>Cabecera</h3><input type="text" name="header" required>
                     <h3>Cuerpo</h3><input type="text" name="rawData" required>
+                    <h3>Categoría</h3><input type="text" id="category" name="category" placeholder="Escribe tu categoría o selecciona una">
+                    <select id="selectCategory" onchange="selectCategoryF()">
+                        <option selected>---</option>
+                    <?php
+                        $resul = selectAllCategory();
+                            foreach ($resul as $row){
+                                echo "<option>".$row["name"]."</option>";
+                            }
+                    ?>
+                    </select>
+                    <br>
                     <input type="submit" name="subQuestion" value="Formular pregunta">
                 </div>
             </form>
         </div>
         <?php
             if (isset($_POST["subQuestion"])) {
-                insertQuestion($_POST["header"], $_POST["rawData"], date("m-d-Y H:i:s"), $_SESSION["user"]["userId"], "1");
+                $idCategory = selectIdCategory($_POST["category"]);
+                if ($idCategory == null) {
+                    insertCategory($_POST["category"]);
+                    $idCategory = selectIdCategory($_POST["category"]);
+                }
+                insertQuestion($_POST["header"], $_POST["rawData"], date("m-d-Y H:i:s"), $_SESSION["user"]["userId"], $idCategory);
             }
         ?>
     </div>
 </body>
+    <script type="text/javascript">
+        function selectCategoryF(){
+            var e = document.getElementById("selectCategory");
+            console.log(e.options[e.selectedIndex].value);
+            if (e.options[e.selectedIndex].value === "---") {
+                document.getElementById("category").value = "";
+            }else{
+                document.getElementById("category").value = e.options[e.selectedIndex].value;
+            }
+        }
+    </script>
     <script src="js/main.js"></script></hmtl>
 </html>
