d979f180644d82fa25e7e8c21b2f23a8cbb347ee
Unknown <rafaestaenclase@gmail.com>
2018-11-16 11:36:02 +0100

    css proceso

--- source/question.php
+++ source/question.php
@@ -6,21 +6,73 @@
     <script src="js/jquery.js"></script>
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/grid.css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Nanum+Gothic" rel="stylesheet">
     <style type="text/css">
+
+        #grid{
+            background-color: #D5F6F0;
+        }
+
+        header{
+            background-color: white;
+            height: auto;
+        }
+
         #content{
-            background-color: lightblue;
-            margin: 0 auto;
-            width: 280px;
+            background-color: white;
+            margin: 10px auto;
+            width: 90%;
+            padding: 0 .5em;
         }
 
         .divQuestion{
             background-color: white;
         }
 
+        #header{
+            font-size: 1.5em;
+        }
+
+        #content input{
+            height: 40px;
+            width: 95%;
+            word-wrap: break-word;
+            border: 0.5px grey solid;
+            
+
+        }
+
+        #content input:disabled{
+            outline: none;
+            border-color: white;
+            box-shadow: none;
+            background-color: white;
+            border:none;
+        }
+
+
+        .divQuestion textarea:disabled{
+            outline: none;
+            border-color: white;
+            box-shadow: none;
+            background-color: white;
+            border:none;
+        }
+
         #rawData{
             width: 100%;
             height: 120px;
         }
+
+        h3{
+            font-family: 'Josefin Sans', sans-serif;
+            margin-bottom: 0px;
+        }
+
+        select{
+            margin: 10px 0;
+        }
+
 
     </style>
 </head>
@@ -39,7 +91,7 @@
         <div id="content">
             <form method="POST">
                 <div class="divQuestion">
-                    <h3>Cabecera</h3><input type="text" id="header" name="header" required>
+                    <h3>Cabecera</h3><input type="text" id="header" name="header" required maxlength="25">
                     <h3>Cuerpo</h3><textarea id="rawData" maxlength="255" name="rawData" required></textarea>
                     <h3>Categoría</h3><input type="text" id="category" name="category" placeholder="Escribe o selecciona catergoría">
                     <select id="selectCategory" onchange="selectCategoryF()">
