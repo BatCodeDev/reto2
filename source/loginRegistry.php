f4beca8ceb456029c7e0859af1ad679450655018
Unknown <rafaestaenclase@gmail.com>
2018-11-20 10:44:46 +0100

    cosas

--- source/loginRegistry.php
+++ source/loginRegistry.php
@@ -9,32 +9,49 @@
 <html>
 <head>
 	<meta name="viewport" content="width=device-width">
-	<title>Login</title>
+	<title>LOGIN</title>
 	<script type="text/javascript" src="src/jquery-3.3.1.js"></script>
 	<script src="js/main.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Nanum+Gothic" rel="stylesheet">
 	<style type="text/css">
 
 		body {
-			display: flex;
-			flex-direction: column;
-			justify-content: space-around;
+            background: linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)), url("img/bg.jpg");
+
 		}
 
 		nav{
-			padding: 5% 0%;
-			justify-content: space-around;
+			padding: 20px 0px;
+			text-align: center;
+		}
+
+		nav img{
+			width: 280px;
+		}
+
+		#content{
+			width: 280px;
+			margin: 0 auto;
+			padding: 10px 0px;
 		}
 
 		.divLoginRegistry{
 			width: 280px;
 
 			margin: 0 auto;
-			margin-top: 5%;
+			margin-top: 15px;
+			padding-top: 3px;
 
 
-			background-color: lightblue;
+			border: dashed 1px grey;
 			text-align: center;
+			background-color: lightblue;
 		}
+
+		p{
+			font-family: 'Josefin Sans', sans-serif;
+		}
+		
 
 
 		.divLoginRegistry input{
@@ -52,40 +69,47 @@
 
 		    font-size: 100%;
 
+		    font-family: 'Nanum Gothic', sans-serif;
 		    text-align: center;
 		    text-transform: lowercase;
 
+
 		    cursor: auto!important;
+		}
+
+		button{
+			background-color: #FC3667;
 		}
 
 	</style>
 </head>
 <body>
 <nav>
+	<img src="img/logo.png">
 </nav>
+		<div id="content">
+			<div id="divLogin" class="divLoginRegistry">
+				<p>LOGIN</p>
+				<p id="errorLogin"></p>
+				<form id="formLogin" method="POST">
+					<input type="text" name="user" placeholder="Usuario" required>
+					<input type="password" name="password" placeholder="Contraseña" required>
+					<input type="submit" name="login" value="Iniciar sesión">
+				</form>
+			</div>
 
-
-	<div id="divLogin" class="divLoginRegistry">
-		<p>Login</p>
-		<p id="errorLogin"></p>
-		<form id="formLogin" method="POST">
-			<input type="text" name="user" placeholder="Usuario" required>
-			<input type="password" name="password" placeholder="Contraseña" required>
-			<input type="submit" name="login" value="Iniciar sesión">
-		</form>
-	</div>
-
-	<div id="divRegistry" class="divLoginRegistry">
-		<p>Registry</p>
-		<p id="errorRegistry"></p>
-		<form id="formRegistry" onsubmit="return validate(this, this.user)" method="POST">
-			<input type="email" name="email" placeholder="Correo" required>
-			<input type="text" name="user" placeholder="Usuario" required>
-			<input type="password" name="password" placeholder="Contraseña" required>
-			<input type="text" name="name" placeholder="Nombre">
-			<input type="text" name="surname" placeholder="Apellido">
-			<input type="submit" name="registry" value="Registrar">
-		</form>
+			<div id="divRegistry" class="divLoginRegistry">
+				<p>REGISTRY</p>
+				<p id="errorRegistry"></p>
+				<form id="formRegistry" onsubmit="return validate(this, this.user)" method="POST">
+					<input type="email" name="email" placeholder="Correo" required>
+					<input type="text" name="user" placeholder="Usuario" required>
+					<input type="password" name="password" placeholder="Contraseña" required>
+					<input type="text" name="name" placeholder="Nombre">
+					<input type="text" name="surname" placeholder="Apellido">
+					<input type="submit" name="registry" value="Registrar">
+				</form>
+			</div>
 	</div>
 </body>
 </html>
