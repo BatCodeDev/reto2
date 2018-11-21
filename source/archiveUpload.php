766e20b00f6688b4695c31a60d20a75de83186af
IFONS <smashpit94@gmail.com>
2018-11-21 13:24:26 +0100

    Add files via upload

--- source/archiveUpload.php
+++ source/archiveUpload.php
@@ -7,7 +7,7 @@
  */
 
 include_once "server/connection.php";
-include_once 'server/ArchiveDB.php';
+include_once 'server/archiveDB.php';
 
 
 /**
@@ -15,23 +15,27 @@ include_once 'server/ArchiveDB.php';
  * Funcion que renombra un archivo y lo sube
  */
 
-function uploadFile($date2)
+
+
+if(isset($_FILES) && !empty($_FILES))
 {
-
-
     if ($_FILES["file2"]["error"] > 0){
         echo "Error: " . $_FILES["file2"]["error"] ."<br />";
     }else{
+        $date2=uniqid();
         $nombreArchivo = $_FILES['file2']['name'];
         $nombreTmpArchivo = $_FILES['file2']['tmp_name'];
         $extension = explode(".", $_FILES['file2']['name']);
         $ext = end($extension);
         $destino = "./img/archive/".$date2.".".$ext;
         move_uploaded_file($nombreTmpArchivo, $destino);
+        //var_dump(die($destino));
         save($destino);
 
     }
 }
+
+
 
 
 
